67c84ac8107df7b1cd3a5236a10740e4526fb653
Athanasios Aris Panagopoulos <apanagopoulos@users.noreply.github.com>
2018-05-09 14:13:39 -0700

    lights
    DR-lights

---
+++ lights.py
@@ -0,0 +1,120 @@
+import time
+from collections import defaultdict
+from xbos import get_client
+from xbos.services.hod import HodClient
+from xbos.devices.light import Light
+from xbos.devices.occupancy_sensor import Occupancy_Sensor
+import pandas as pd
+from xbos.services import mdal
+import datetime, pytz
+from datetime import timedelta
+
+############################################### Initializing our datasets
+SITE = "csu-dominguez-hills" #Cahnge this according to the site
+RoomsType = pd.read_csv('CSUDH_Room-Types-Lighting-Reqs.csv', index_col=2, header=[0]) #change this according to the site (keep the same format and header names)
+
+#print RoomsType.RoomsType[df['column_name'] == some_value]
+c = get_client()
+hod = HodClient("xbos/hod")
+
+skipped = ""
+
+####### occ query
+
+occ_query = """SELECT * FROM %s WHERE {
+?l rdf:type brick:Lighting_System .
+?l bf:feeds ?room .
+?room rdf:type brick:Room .
+?l bf:uri ?luri .
+?l bf:hasPoint ?p .
+?p rdf:type brick:Occupancy_Sensor .
+?p bf:uri ?puri .
+?p bf:uuid ?puuid
+};"""
+
+res2=hod.do_query(occ_query % SITE)
+
+byrooms_o = defaultdict(list)
+for item in res2['Rows']:
+	byrooms_o[item['?room']].append(item['?puuid'])
+
+print byrooms_o
+print "Occupany uuids loaded!"
+
+
+####### light query
+
+
+light_query = """
+SELECT ?light ?room ?light_uri FROM %s WHERE {
+  ?light rdf:type brick:Lighting_System .
+  ?light bf:feeds ?room .
+  ?room rdf:type brick:Room .
+  ?light bf:uri ?light_uri
+};
+"""
+
+res = hod.do_query(light_query % SITE)
+
+byrooms = defaultdict(list)
+all_rooms = set()
+for item in res['Rows']:
+	all_rooms.add(item['?room'])
+	try:
+		l = Light(c, item['?light_uri'])
+		byrooms[item['?room']].append(l)
+	except:
+		skipped += str(item['?light'])+"in room"+str(item['?room'])+" skipped! \n"
+		pass
+
+
+print "Lighting systems loaded!"
+
+################################################ Controls
+
+c = mdal.MDALClient("xbos/mdal")
+
+for room in all_rooms:
+	Type = RoomsType["Room Type"][room][0]
+	if Type=="Hallway":
+		for light in byrooms[room]:
+			print "a"#print light.brightness
+			#light.set_brightness(20)
+
+	if Type=="Toilet":
+		for light in byrooms[room]:
+			print "c"#print light.brightness
+			#light.set_brightness(10)
+
+	if Type!="Hailway":
+		query_list = []
+		for i in byrooms_o[room]:
+			query_list.append(i)
+
+		# get the sensor data
+		now= datetime.datetime.utcnow().replace(tzinfo=pytz.timezone("UTC"))
+		dfs = c.do_query({'Composition': query_list,
+						  'Selectors': [mdal.MAX] * len(query_list),
+						  'Time': {'T0': (now - timedelta(hours=0.5)).strftime('%Y-%m-%d %H:%M:%S') + ' UTC',
+								   'T1': now.strftime('%Y-%m-%d %H:%M:%S') + ' UTC',
+								   'WindowSize': str(30) + 'min',
+								   'Aligned': True}})
+
+		dfs = pd.concat([dframe for uid, dframe in dfs.items()], axis=1)
+
+		df = dfs[[query_list[0]]]
+		df.columns.values[0] = 'occ'
+		df.is_copy = False
+		df.columns = ['occ']
+		# perform OR on the data, if one sensor is activated, the whole room is considered occupied
+		for i in range(1, len(query_list)):
+			df.loc[:, 'occ'] += dfs[query_list[i]]
+		df.loc[:, 'occ'] = 1 * (df['occ'] > 0)
+
+		if df["occ"][0]!=1:
+			print "a"#light.brightness
+			#light.set_brightness(0)
+
+print "Done! We skipped the following sensors:"
+print skipped
+
